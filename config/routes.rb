a842c9225cf0546c8d546dcb18a0d047f1af44c1
Patcharapol Nirunpornputta <patcharapol.n@ku.th>
2017-05-25 11:30:03 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-    root 'static_pages#home'
-    get '/home', to: 'static_pages#home'
-    get '/about', to: 'static_pages#about'
-    get '/academics', to: 'static_pages#academics'
-    get '/contact', to: 'static_pages#contact'
-    get '/research', to: 'static_pages#research'
-    get '/researchLab', to: 'static_pages#researchLab'
-    get '/scholarship', to: 'static_pages#scholarship'
+    root :to => redirect('/home.html')
 end
