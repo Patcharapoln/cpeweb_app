1ebc8bee928d063ec37b7baeaca6e4dfb13911d5
Patcharapol Nirunpornputta <patcharapol.n@ku.th>
2017-05-25 06:47:20 +0000

    Add new pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+  def home
+  end
+  def contact
+  end
+  def acedemics
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/About'
+
+  get 'static_pages/home'
+
+    root 'static_pages#hello'
+    get 'static_pages/home'
+    get 'static_pages/about'
+    get 'static_pages/academics'
+    get 'static_pages/contact'
 end
