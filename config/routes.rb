f514f2c5478567c11521f508b3a78b42c671df3a
Issaree Srisomboon <issaree.sr@ku.th>
2017-05-25 09:49:27 +0000

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     get '/about', to: 'static_pages#about'
     get '/academics', to: 'static_pages#academics'
     get '/contact', to: 'static_pages#contact'
-
+    get '/research', to: 'static_pages#research'
+    get '/researchLab', to: 'static_pages#researchLab'
+    get '/scholarship', to: 'static_pages#scholarship'
 end
